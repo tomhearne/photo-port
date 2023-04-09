9c350b262ef52619e8139e485f8041a88b1f54e8
Thomas Hearne <thomas.hearne@motominer.com>
2023-04-09 16:55:49 -0500

    Conditional contact page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: 'commercial',
@@ -24,13 +25,18 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
+        {!contactSelected ? (
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
           <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
-        </div>
+        )}
       </main>
     </div>
   );
