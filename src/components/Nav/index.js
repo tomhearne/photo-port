163e79b9a9aa28c02ca977575418afc2e1d7e0b5
Thomas Hearne <thomas.hearne@motominer.com>
2023-04-08 23:42:31 -0500

    finished photolist

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -23,9 +23,7 @@ function Nav(props) {
         <nav>
           <ul className="flex-row">
             <li className="mx-2">
-              <a
-                href="#about"
-              >
+            <a data-testid="about" href="#about">
                 About me
               </a>
             </li>
@@ -33,15 +31,12 @@ function Nav(props) {
               <span>Contact</span>
             </li>
             {categories.map((category) => (
-              <li className={`mx-1 ${
-                  currentCategory.name === category.name && 'navActive'
-                  }`} key={category.name}>
-                <span
-                  onClick={() => {
-                    setCurrentCategory(category)
-                  }}
-                >
-                  {capitalizeFirstLetter(category.name)}
+              <li
+                className="mx-1"
+                key={category.name}
+              >
+                <span onClick={() => categorySelected(category.name)} >
+                  {category.name}
                 </span>
               </li>
             ))}
